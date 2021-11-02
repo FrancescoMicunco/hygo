d72fb8da3b2508f7c0b6298e19a364eb9e726332
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-11-02 13:53:37 +0100

    path images

--- counter.js
+++ counter.js
@@ -9,7 +9,6 @@ setInterval(() => {
     let counterCo2 = todaySecond * 32.4;
     let counterEuri = todaySecond * 0.00705 * 60;
     let counterLitri = todaySecond * 0.0176 * 60;
-
     co2.innerText = `${Math.floor(counterCo2)}`
     euri.innerText = `${Math.floor(counterEuri)}`
     litri.innerText = `${Math.floor(counterLitri)}`
--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-fetch("https://air-quality.p.rapidapi.com/current/airquality?lon=-73.00597&lat=40.71427", {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "air-quality.p.rapidapi.com",
-            "x-rapidapi-key": "6672120cbcmshc0572882b285990p1c05d5jsn50b707427d46"
-        }
-    })
-    .then(response => {
-        console.log(response);
-    })
-    .catch(err => {
-        console.error(err);
-    });
-
 $('#myModal').on('shown.bs.modal', function() {
     $('#myInput').trigger('focus')
 })
