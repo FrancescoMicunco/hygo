645f7933a784deb199b1013fb57c7f529f09d90a
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-11-02 10:17:34 +0100

    counter

--- counter.js
+++ counter.js
@@ -1,5 +1,16 @@
-let counter = 100;
+let todayDate = new Date();
+//data di partenza (20 luglio 2021)
+let todaySecond = 9676800 + (todayDate.getSeconds() * 60 * todayDate.getHours())
 let co2 = document.getElementById("co2")
+let euri = document.getElementById("euri")
+let litri = document.getElementById("litri")
 setInterval(() => {
-    co2.innerText = `${counter}`
+    todaySecond += 1
+    let counterCo2 = todaySecond * 32.4;
+    let counterEuri = todaySecond * 0.00705 * 60;
+    let counterLitri = todaySecond * 0.0176 * 60;
+    console.log(counterEuri)
+    co2.innerText = `${Math.floor(counterCo2)}`
+    euri.innerText = `${Math.floor(counterEuri)}`
+    litri.innerText = `${Math.floor(counterLitri)}`
 }, 1000);
